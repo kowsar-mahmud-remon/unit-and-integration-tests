b421006a81a0ffdc9162444b8251089aad8efbbc
creativerze <creativerze@gmail.com>
2023-11-06 12:25:35 +0600

    added 3 test file

--- main.test.js
+++ main.test.js
@@ -3,7 +3,50 @@ import { add } from "./math";
 
 
 it("should return the correct sum if an array of number is provided", () => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  //simple way
+  // const result = add([1, 2, 3]);
+  // expect(result).toBe(6);
+
+  // AAA way 
+  // Arrange
+  const number = [1, 2, 3];
+
+  const expertedResult = number.reduce((acc, cur) => acc + cur);
+
+  // Action
+  const result = add(number);
+
+  // Assertion
+  expect(result).toBe(expertedResult);
+
+});
+
+
+it("it should provide NaN if at least one invalid number is provided", () => {
+
+  // Arrange
+  const number = [1, "invalid", 3];
+
+  // Action
+  const result = add(number);
+
+  // Assertion
+  expect(result).toBeNaN();
+
+});
+
+it("it should provide correct sum if an array numeric string is provided", () => {
+
+  // Arrange
+  const number = ["1", "2", "3"];
+
+  const expertedResult = number.reduce((acc, cur) => +acc + +cur);
+
+  // Action
+  const result = add(number);
+
+  // Assertion
+  expect(result).toBe(expertedResult);
+
 })
 
